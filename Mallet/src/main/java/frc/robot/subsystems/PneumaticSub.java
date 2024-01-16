a9f5301927d43242f3119875fa35161ecf7e9345
phomebrew <stevenhsiao4@gmail.com>
2024-01-15 19:07:42 -0800

    Added SmartDashboard for PneumaticSub

--- Mallet/src/main/java/frc/robot/subsystems/PneumaticSub.java
+++ Mallet/src/main/java/frc/robot/subsystems/PneumaticSub.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import frc.robot.Constants.K_PneumaticSub;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PneumaticSub extends SubsystemBase {
@@ -40,5 +41,22 @@ public class PneumaticSub extends SubsystemBase {
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
+        if(K_PneumaticSub.isUsingPneumatic)
+        {
+            String dSolenoidStatus = "None";
+            if(doubleSolenoid.get() == DoubleSolenoid.Value.kForward)
+                dSolenoidStatus = "Forward";
+            if(doubleSolenoid.get() == DoubleSolenoid.Value.kReverse)
+                dSolenoidStatus = "Reverse";
+            if(doubleSolenoid.get() == DoubleSolenoid.Value.kOff)
+                dSolenoidStatus = "Off";
+            SmartDashboard.putNumber("Pressure PSI", compressor.getPressure());
+            SmartDashboard.putBoolean("Pressure Status", compressor.getPressureSwitchValue());
+            SmartDashboard.putNumber("Pressure Current Drawn", compressor.getCurrent());
+            SmartDashboard.putString("Double Solenoid Status", dSolenoidStatus);
+            System.out.println("\nPressure Status: " + compressor.getPressureSwitchValue() + 
+                "\nPressure PSI: " + compressor.getPressure() + "\nPressure Current Drawn: " + 
+                compressor.getCurrent() + "\nDouble Solenoid: " + dSolenoidStatus + "\n");
+        }
     }
 }
